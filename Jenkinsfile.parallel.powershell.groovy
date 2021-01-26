ae25b88bcb5b710b23dc53884fcfd3b25bf34817
Vardan <hvardan71@gmail.com>
2021-01-25 18:33:29 -0800

    test
    testing

--- Jenkinsfile.parallel.powershell.groovy
+++ Jenkinsfile.parallel.powershell.groovy
@@ -10,6 +10,7 @@ pipeline {
                     steps {
                         powershell '''
                             hostname
+                            pwd
                         '''
                     }
                     post {
