97ceadf4966df5a3f7a9a161b7fb03605a268396
Vardan <hvardan71@gmail.com>
2021-01-25 20:29:22 -0800

    Update Jenkinsfile.parallel.powershell.groovy

--- Jenkinsfile.parallel.powershell.groovy
+++ Jenkinsfile.parallel.powershell.groovy
@@ -35,7 +35,7 @@ pipeline {
                 stage('Run another job') {
                     steps {
                         build job: 'Job_With_Parameters-pipeline', 
-                            parameters: [[$class: 'StringParameterValue', name: 'String', value: 'Run another job: wait false']], wait: false
+                            parameters: [[$class: 'StringParameterValue', name: 'String', value: 'Run another job: wait false']], wait: true
                         build job: 'Job_With_Parameters-pipeline', 
                             parameters:[
                                 string(name: 'String', value: 'testing' ),
