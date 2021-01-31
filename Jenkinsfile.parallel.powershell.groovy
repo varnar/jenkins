273000dc016e9d092bbddaed2945dedf4de27f60
Vardan <hvardan71@gmail.com>
2021-01-31 10:56:12 -0800

    b

--- Jenkinsfile.parallel.powershell.groovy
+++ Jenkinsfile.parallel.powershell.groovy
@@ -21,7 +21,6 @@ pipeline {
                             ''' )
                         }
                         println SleepTime
-                        
                     }
                     post {
                         always {
