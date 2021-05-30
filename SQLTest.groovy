4adee375fe3c83868b655acd795ab01c2d633156
Vardan <varnar@users.noreply.github.com>
2021-05-30 15:15:42 -0700

    Create SQLTest.groovy

---
+++ SQLTest.groovy
@@ -0,0 +1,10 @@
+import groovy.sql.Sql
+import jenkins.model.*
+
+nodes = Jenkins.instance.globalNodeProperties
+nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+Server = nodes[0].envVars.get("GLOBAL_DE_SQL_SERVER","unknown")
+sql = Sql.newInstance("jdbc:sqlserver://$Server;integratedSecurity=true", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+datacenters = sql.rows("exec [TechOps].[env].[GetDataCenters] @IncludeOnly = '', @StatusId = ''").collect({ query -> query.Code})
+datacenters .add(0,'')
+return datacenters 
