f94e93b523237277077b8bbb72b536a40c057dd6
Vardan <varnar@users.noreply.github.com>
2021-05-30 15:23:14 -0700

    Update SQLTest.groovy

--- SQLTest.groovy
+++ SQLTest.groovy
@@ -6,5 +6,5 @@ nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
 Server = nodes[0].envVars.get("GLOBAL_DE_SQL_SERVER","unknown")
 sql = Sql.newInstance("jdbc:sqlserver://$Server;integratedSecurity=true", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
 datacenters = sql.rows("exec [TechOps].[env].[GetDataCenters] @IncludeOnly = '', @StatusId = ''").collect({ query -> query.Code})
-datacenters .add(1,'')
+//datacenters .add(1,'')
 return datacenters 
